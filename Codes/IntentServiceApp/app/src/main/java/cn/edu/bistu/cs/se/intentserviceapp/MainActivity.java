1c85d9d23a414c920df87ef12c0f80def6283e42
Haobaoshui <hbs@bistu.edu.cn>
2015-10-24 02:19:54 +0800

    Version 1.2
    update codes.

---
+++ Codes/IntentServiceApp/app/src/main/java/cn/edu/bistu/cs/se/intentserviceapp/MainActivity.java
@@ -0,0 +1,74 @@
+package cn.edu.bistu.cs.se.intentserviceapp;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Button buttonService1=(Button)findViewById(R.id.btnService1);
+        Button buttonService2=(Button)findViewById(R.id.btnService2);
+
+        buttonService1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyIntentService.startActionFoo(MainActivity.this, "1", "2");
+            }
+        });
+
+        buttonService2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyIntentService.startActionBaz(MainActivity.this, "3", "4");
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
