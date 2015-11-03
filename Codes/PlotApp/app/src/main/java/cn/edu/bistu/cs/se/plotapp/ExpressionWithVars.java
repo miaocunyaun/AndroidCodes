564717d987e50ba919f759da685927d6204515fc
Haobaoshui <hbs@bistu.edu.cn>
2015-11-03 12:45:30 +0800

    Version 1.2.3
    update codes

---
+++ Codes/PlotApp/app/src/main/java/cn/edu/bistu/cs/se/plotapp/ExpressionWithVars.java
@@ -0,0 +1,29 @@
+package cn.edu.bistu.cs.se.plotapp;
+
+/**
+ * Created by hbs on 2015-11-2.
+ */
+public class ExpressionWithVars extends  Expression{
+    private String m_strExp;
+    private String m_strVar;
+
+    public ExpressionWithVars(String strExp, String var)
+    {
+        m_strExp = strExp;
+        m_strVar = var;
+    }
+
+    public double evalf(double dVarVal)
+    {
+
+        String str = m_strExp.replace(m_strVar, dVarVal+"");
+
+
+        double d = super.evalf(str);
+        return d;
+
+    }
+
+
+
+}
